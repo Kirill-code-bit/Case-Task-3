713f427b39a39987bba8e42d4b3b7c030732f6f1
Kirill-code-bit <kirillvdovin911@gmail.com>
2024-12-18 17:22:48 +0300

    Everything is loaded
    functional page

---
+++ main.js
@@ -0,0 +1,19 @@
+document.getElementById('contact-form')
+document.addEventListener('submit', function (event) {
+		const name = document.getElementById('name').value;
+		const email = document.getElementById('email').value;
+		const message = document.getElementById('message').value;
+
+		if (!name || !email || !message) {
+			alert('Все поля должны быть заполнены!');
+			event.preventDefault(); // Предотвращает отправку формы
+		} else if (!validateEmail(email)) {
+			alert('Введите корректный адрес электронной почты!');
+			event.preventDefault(); // Предотвращает отправку формы
+		}
+	});
+
+function validateEmail(email) {
+	const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return re.test(email);
+}
